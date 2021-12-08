2dc7fdc920c1d8a2dfbf4bce21386058f5caad86
GabrielTabilo <gabriel.tabilo51@gmail.com>
2021-12-08 19:06:17 -0300

    thirteenth commit: button widget

--- pages/templates.vue
+++ pages/templates.vue
@@ -1,104 +1,61 @@
 <template>
+
   <div class="row">
-    <div class="col-4">
-      <iot-indicator :config="config1"></iot-indicator>
-    </div>
-    <div class="col-4">
-      <iot-indicator :config="config2"></iot-indicator>
-    </div>
-    <div class="col-4">
-      <iot-indicator :config="config3"></iot-indicator>
-    </div>
-    <br><br>
-    <div style="margin-top: 200px;">
-        <button @click="sendData1()">SEND userid/8888/var1/sdata</button>
-        <button @click="sendData2()">SEND userid/8888/var2/sdata</button>
-        <button @click="sendData3()">SEND userid/8888/var3/sdata</button>
 
-
-    </div>
-
+    <iot-button :config="configButton"></iot-button>
 
   </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                value: true,
-                config1: {
-                  userId: "userid",
-                  selectedDevice: {
-                    name: "Home",
-                    dId: "8888",
-                    templateName: "Power Sensor",
-                    templateId: "984237562348756ldksjfh",
-                    saverRule: false
-                  },
-                  variableFullName: "Pump",
-                  variable: "var1",
-                  icon: "fa-sun",
-                  column: "col-6",
-                  widget: "indicator",
-                  class: "success"
-                },
-                config2: {
-                  userId: "userid",
-                  selectedDevice: {
-                    name: "Home",
-                    dId: "8888",
-                    templateName: "Power Sensor",
-                    templateId: "984237562348756ldksjfh",
-                    saverRule: false
-                  },
-                  variableFullName: "Light",
-                  variable: "var2",
-                  icon: "fa-sun",
-                  column: "col-6",
-                  widget: "indicator",
-                  class: "success"
-                },
-                  config3: {
-                  userId: "userid",
-                  selectedDevice: {
-                    name: "Home",
-                    dId: "8888",
-                    templateName: "Power Sensor",
-                    templateId: "984237562348756ldksjfh",
-                    saverRule: false
-                  },
-                  variableFullName: "Engine",
-                  variable: "var3",
-                  icon: "fa-sun",
-                  column: "col-6",
-                  widget: "indicator",
-                  class: "success"
-               }
-            }
+export default {
+  data() {
+    return {
+      value: false,
+
+      configButton:{
+        userId: "userid",
+        selectedDevice: {
+          name: "Home",
+          dId: "8888",
+          templateName: "Power Sensor",
+          templateId: "984237562348756ldksjfh",
+          saverRule: false
         },
-        methods: {
-          sendData1() {
-            this.value = !this.value;
-            const toSend = {
-              value: this.value
-            };
-            this.$nuxt.$emit("userid/8888/var1/sdata", toSend);
-          },
-          sendData2() {
-            this.value = !this.value;
-            const toSend = {
-              value: this.value
-            };
-            this.$nuxt.$emit("userid/8888/var2/sdata", toSend);
-          },
-          sendData3() {
-            this.value = !this.value;
-            const toSend = {
-              value: this.value
-            };
-            this.$nuxt.$emit("userid/8888/var3/sdata", toSend);
-          }
-        }
-    }
+        variableFullName: "Pump",
+        variable: "var1",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "danger",
+        message: "{'fanstatus': 'stop'}"
+      },
+
+      configIndicator: {
+        userId: "userid",
+        selectedDevice: {
+          name: "Home",
+          dId: "8888",
+          templateName: "Power Sensor",
+          templateId: "984237562348756ldksjfh",
+          saverRule: false
+        },
+        variableFullName: "Pump",
+        variable: "var1",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "success"
+      },
+
+
+    };
+  },
+
+  methods: {
+
+
+
+  }
+};
 </script>
