a075465033db21676f98c18f1f5e06e98a0b2aa5
GabrielTabilo <gabriel.tabilo51@gmail.com>
2021-12-13 06:49:03 -0300

    twenty-first commit: mongodb connection

--- api/index.js
+++ api/index.js
@@ -31,3 +31,42 @@ app.listen(3001, () => {
 
 //"/testing" endpopoint ahora se encuentra routes/devices.js
 
+//Mongo Connection
+const mongoUserName = "devuser";
+const mongoPassword = "devpassword";
+const mongoHost = "localhost";
+const mongoPort = "27017";
+const mongoDatabase = "ioticos_god_level";
+
+var uri = "mongodb://" + mongoUserName + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  authSource: "admin"
+};
+
+try { //intentar
+  mongoose.connect(uri, options).then(
+      () => {
+        console.log("\n");
+        console.log("*******************************".green);
+        console.log("✔ Mongo Successfully Connected!".green);
+        console.log("*******************************".green);
+        console.log("\n");
+      },
+      (err) => {
+        console.log("\n");
+        console.log("*******************************".red);
+        console.log("    Mongo Connection Failed    ".red);
+        console.log("*******************************".red);
+        console.log("\n");
+        console.log(err);
+      }
+    );
+} catch (error) {
+  console.log("ERROR CONNECTING MONGO ");
+  console.log(error);
+}
